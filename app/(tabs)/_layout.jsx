183d21fdf2ddb8919d84bffe71215f10bc0057b0
VitorGPorto <you@ex>
2024-08-06 21:51:29 -0300

    Criando barra do menu e colocar primeiras cores

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,52 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+import {Colors} from '../../constants/Colors'
+
+const _layout = () => {
+  return (
+   <Tabs screenOptions={{
+    headerShown:false,
+    tabBarActiveTintColor: Colors.PRIMARY
+    }}> 
+    <Tabs.Screen name='home'
+      options={{
+        tabBarLabel:'Home',
+        tabBarIcon: ({color})=>
+        <Ionicons 
+            name="home" 
+            size={24} 
+            color={color} 
+
+            />
+      }}
+    />
+    <Tabs.Screen name='explore'
+       options={{
+        tabBarLabel:'Explore',
+        tabBarIcon: ({color})=>
+          <Ionicons 
+            name="search" 
+            size={24} 
+            color={color} />
+      }}
+    />
+    <Tabs.Screen name='profile'
+       options={{
+        tabBarLabel:'Profile',
+        tabBarIcon: ({color})=>
+          <Ionicons 
+            name="people-sharp" 
+            size={24} 
+            color={color}
+            />
+      }}
+    />
+   
+   </Tabs>
+  )
+}
+
+export default _layout
---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const home = () => {
+  return (
+    <View>
+      <Text style={{fontSize:40, fontFamily:'outfit'}}>home</Text>
+    </View>
+  )
+}
+
+export default home
